93edfb4a21e64ea1e1346ca8ab046f668afd3045
Devansh Singh <devansh7865@gmail.com>
2024-05-24 17:20:11 +0530

    Updates

---
+++ precache-manifest.63a613bedbcbbb18785210988ccf5180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6569d08a00e0e84164ac46ef2793b09",
+    "url": "/Dev12820/AIassistednewsapp/index.html"
+  },
+  {
+    "revision": "360f81c8dbda3840a38f",
+    "url": "/Dev12820/AIassistednewsapp/static/css/main.69d521fe.chunk.css"
+  },
+  {
+    "revision": "c6c1b32682454841e5ea",
+    "url": "/Dev12820/AIassistednewsapp/static/js/2.1eca2770.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Dev12820/AIassistednewsapp/static/js/2.1eca2770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360f81c8dbda3840a38f",
+    "url": "/Dev12820/AIassistednewsapp/static/js/main.2315ad03.chunk.js"
+  },
+  {
+    "revision": "9067b714bd18e67339d9",
+    "url": "/Dev12820/AIassistednewsapp/static/js/runtime-main.dd8dd665.js"
+  },
+  {
+    "revision": "7062fe59e1a1cdadf879aeb7787b759d",
+    "url": "/Dev12820/AIassistednewsapp/static/media/logo.7062fe59.png"
+  }
+]);
